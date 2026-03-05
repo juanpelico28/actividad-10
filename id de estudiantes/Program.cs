7cedabb75fee8df494f2d4035160d2258d0e5edc
Juan Pelicò <ref.juanlopez@gmail.com>
2026-03-04 23:25:44 -0600

    ejerc 5

--- id de estudiantes/Program.cs
+++ id de estudiantes/Program.cs
@@ -10,29 +10,33 @@ namespace id_de_estudiantes
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> ciudades = new Dictionary<int, string>();
-            int codigo ;
-            string nombre;
-            Console.WriteLine("++++++++++++++ INGRESAR 5 CIUDADES +++++++++++");
-            for (int i = 0; i < 5; i++)
+            Dictionary<string, string> usuarios = new Dictionary<string, string>();
+            string contraseña;
+            string usuario;
+           
+            Console.Write("CUANTOS USUARIOS DESEA INGRESAR : ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n++++++++++++++ INGRESAR DATOS DE USUARIO +++++++++++");
+            for (int i = 0; i < num; i++)
             {
-                Console.Write($"\nINGRESE EL CODIGO DE LA CIUDAD {i + 1}: ");
-                codigo = int.Parse(Console.ReadLine());
-                Console.Write($"INGRESE EL NOMBRE DE LA CIUDAD {i + 1}: ");
-                nombre = Console.ReadLine();
-                ciudades.Add(codigo, nombre);
+                Console.Write($"\nINGRESE USUARIO {i + 1}: ");
+                usuario = Console.ReadLine();
+                Console.Write($"INGRESE LA CONTRASEÑA DEL USUARIO {i + 1}: ");
+                contraseña = Console.ReadLine();
+
+                usuarios.Add(usuario, contraseña);
             }
             Console.Clear();
-            Console.WriteLine("\n++++++++++++++ BUSCAR CIUDAD +++++++++++");
-            Console.Write("INGRESE EL CODIGO DE LA CIUDAD: ");
-            codigo = int.Parse(Console.ReadLine()) ;
-            foreach (var item in ciudades)
+            Console.WriteLine("\n++++++++++++++ DESPLEGAR CONTRASEÑA +++++++++++");
+            Console.Write("INGRESE EL USUARIO: ");
+            usuario = Console.ReadLine();
+            foreach (var item in usuarios)
             {
-                if (item.Key == codigo)
+                if (item.Key == usuario)
                 {
-                    Console.WriteLine("CODIGO: " + item.Key + ", NOMBRE: " + item.Value);
+                    Console.WriteLine("\nCONTRASEÑA: " + item.Value);
                 }
-                
+
             }
         }
     }
