9fa84501707bc9c45fc1f8fa2cdb0b63c4ad7562
Juan Pelicò <ref.juanlopez@gmail.com>
2026-03-10 14:30:51 -0600

    ejercicio 10

--- id de estudiantes/Program.cs
+++ id de estudiantes/Program.cs
@@ -10,30 +10,38 @@ namespace id_de_estudiantes
     {
         static void Main(string[] args)
         {
-            Dictionary<string, double> estudiantes = new Dictionary<string, double>();
-            double id;
-            string nombre;
-            
-            Console.WriteLine("\n++++++++++++++  DATOS ESTUDIANTES +++++++++++");
-            estudiantes.Add("juan", 87);
-            estudiantes.Add("pedro",90 );
-            estudiantes.Add("jose", 87);
-            estudiantes.Add("roberto", 75);
-            estudiantes.Add("mario", 66);
-            estudiantes.Add("carlos", 78);
-            Console.WriteLine("\nEMPLEADOS AGREGADOS CORRECTAMENTE.");
-            Console.Write("\nINGRESE ELL NOMBRE DEL ESTUDIANTE: ");
-            nombre = Console.ReadLine();
-            foreach (var item in estudiantes)
-            {
-                if (estudiantes.ContainsKey(nombre))
-                {
-                    Console.WriteLine("EL NOMBRE SI EXISTE. ");
-                    Console.WriteLine("NOMBRE: " + item.Key + "/ NOTA: " + item.Value);
+            Dictionary<int, (string nombre, double precio)> productos = new Dictionary<int, (string, double)>();
+            int  id;
 
-                }
+            Console.WriteLine("\n++++++++++++++  DATOS DE LOS PRODUCTOS +++++++++++");
+            productos.Add(12, (" YOGUR LT. ", 15));
+            productos.Add(15, ("FRIJOLES LB. ", 18));
+            productos.Add(17, ("LECHE NESTLE. ", 20));
+            productos.Add(11, ("GELATINA. ",12));
+            productos.Add(18, ("AZUCAR LB. ", 10));
+            productos.Add(19, ("PAPEL PAQUETE. ", 12));
+            productos.Add(20, ("SOPA INSTANTANEA. ", 5 ));
+            foreach (var item in productos)
+            {
+                Console.WriteLine("ID: " + item.Key + " NOMBRE: " + item.Value.nombre + " PRECIO: Q" + item.Value.precio);
+            }
+
+            Console.WriteLine("\n*****ELIMINAR PRODUCTO DE LA LISTA. ***********");
+            Console.Write("\nINGRESE LA ID DEL PRODUCTO: ");
+            id = int.Parse(Console.ReadLine());
+            
+            if (productos.ContainsKey(id))
+            {
+                    productos.Remove(id);  
+                    Console.WriteLine("SE A ELIMINADO EL PRODUCTO. ");
             }
             Console.ReadKey();
+            Console.WriteLine();
+            foreach(var item in productos)
+            {
+                Console.WriteLine("ID: "+ item.Key+ " NOMBRE: "+item.Value.nombre+ " PRECIO: Q"+ item.Value.precio);
+            }
+            
         }
     }
 }
